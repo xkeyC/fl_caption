166786192f6119d46d73dd29cec286270e7161b3
xkeyC <3334969096@qq.com>
2025-07-01 21:03:24 +0800

    feat: remove sherpa

--- rust/src/onnx/sense_voice_onnx.rs
+++ rust/src/onnx/sense_voice_onnx.rs
@@ -1,9 +1,5 @@
 use crate::whisper_caption::{self, whisper::Segment};
 
-use sherpa_rs::{
-    sense_voice::{SenseVoiceConfig, SenseVoiceRecognizer},
-};
-
 pub fn launch_caption<F>(
     params: whisper_caption::LaunchCaptionParams,
     mut result_callback: F,
@@ -11,13 +7,6 @@ pub fn launch_caption<F>(
 where
     F: FnMut(Vec<Segment>) + Send + 'static,
 {
-       let config = SenseVoiceConfig {
-        model: "sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/model.int8.onnx".into(),
-        tokens: "sherpa-onnx-sense-voice-zh-en-ja-ko-yue-2024-07-17/tokens.txt".into(),
-        provider: Some(provider),
-
-        ..Default::default()
-    };
     // Implement the ONNX model inference logic here
     Ok(())
 }
